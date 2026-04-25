836fccfb55796dd2a62629b74733ce28694c4dc7
tharunthej <tharunthej989@gmail.com>
2026-04-25 22:08:49 +0530

    Add initial domain entities

---
+++ backend/DineCloud/DineCloud.Domain/Entities/MealType.cs
@@ -0,0 +1,16 @@
+﻿
+namespace DineCloud.Domain.Entities
+{
+    public class MealType
+    {
+        public int Id { get; set; }
+
+        public required string Name { get; set; }
+        public decimal CurrentPrice { get; set; }
+
+        public bool IsActive { get; set; }
+
+        // Navigation
+        public ICollection<MealBooking> MealBookings { get; set; } = new List<MealBooking>();
+    }
+}
---
+++ backend/DineCloud/DineCloud.Domain/Entities/Role.cs
@@ -0,0 +1,13 @@
+﻿
+namespace DineCloud.Domain.Entities
+{
+    public class Role
+    {
+        public int Id { get; set; }
+
+        public required string Name { get; set; }
+
+        // Navigation
+        public ICollection<Employee> Employees { get; set; } = new List<Employee>();
+    }
+}
